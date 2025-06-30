0f00b264c09371d8ebc6d6d06c9449859b4ab4b2
cedchua <cedchuaa0324@gmail.com>
2025-06-30 23:44:58 +0800

    6/30/25

--- app/Http/Controllers/ShopOrderTransactionController.php
+++ app/Http/Controllers/ShopOrderTransactionController.php
@@ -461,7 +461,7 @@ class ShopOrderTransactionController extends Controller
              'shop_order_transaction.updated_at', 'shop_order_transaction.is_pickup',  'shop.shop_name', 'shop.shop_type_id',
              'c.first_name as requestor_name', 'shop_order_transaction.checker', 'shop_order_transaction.requestor',
               'shop_order_transaction.status', 'shop_order_transaction.date', 'shop_order_transaction.profit',
-              'shop_order_transaction.total_cash', 'shop_order_transaction.total_online', 'ct.customer_type', 'shop_order_transaction.rider_name')    
+              'shop_order_transaction.total_cash', 'shop_order_transaction.total_online', 'ct.customer_type', 'shop_order_transaction.rider_name', 'so.shop_order_quantity')    
              ->where('shop.shop_type_id', 3)
              ->where('so.product_id', $id)
              ->orderBy('shop_order_transaction.id', 'DESC')
