edd537a58f1fdce8d71f2cb90d4f42be396ac167
Ced Chua <cedchuaa0324@gmail.com>
2025-08-02 15:55:28 +0800

    test

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -30,6 +30,8 @@ class ShopController extends Controller
         return response()->json($data);   
     }
 
+    
+
         public function fetchShopActive()
     {
         $data = DB::table('shop')
@@ -38,6 +40,17 @@ class ShopController extends Controller
             'shop_type.shop_type_description' , 'shop.status', 'shop.address', 'shop.contact_number')
           ->where('shop.status', 1)  
           ->get();
+        return response()->json($data);  
+    }
+
+       public function fetchCurrentShop()
+    {
+        $data = DB::table('shop')
+          ->join('shop_type', 'shop.shop_type_id', '=', 'shop_type.id')
+          ->select('shop.id', 'shop.shop_name','shop.shop_type_id',
+            'shop_type.shop_type_description' , 'shop.status', 'shop.address', 'shop.contact_number')
+          ->where('shop.status', 1)  
+          ->first();
         return response()->json($data);  
     }
 
